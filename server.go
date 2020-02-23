925ea99fa9409caa4270f31d9ad61bd6d510c30f
Aleksandr Postnikov <sasha2011-1997@mail.ru>
2020-02-23 19:32:36 +0300

    Add registration function

--- server.go
+++ server.go
@@ -1,95 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
-	"math/rand"
 	"net/http"
-	"time"
 
 	"github.com/gorilla/mux"
 )
 
-const cookieSessionName = "SCDSESSIONID"
-
-type LoginInput struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
-
-type SessionHandler struct {
-	sessions  map[string]uint
-	userStore *UserStore
-}
-
 type RestaurantsHandler struct {
-}
-
-var (
-	letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
-)
-
-func GenerateSessionCookie() string {
-	byteSlice := make([]rune, 64)
-	for i := range byteSlice {
-		byteSlice[i] = letterRunes[rand.Intn(len(letterRunes))]
-	}
-	return string(byteSlice)
-}
-
-func (api *SessionHandler) SessionHandle(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		decoder := json.NewDecoder(r.Body)
-		loginInput := new(LoginInput)
-		err := decoder.Decode(loginInput)
-		if err != nil {
-			log.Printf("Error while unmarshalling JSON: %s", err)
-			http.Error(w, `Invalid JSONData`, 400)
-			return
-		}
-
-		id, user, ok := api.userStore.GetUserByEmail(loginInput.Email)
-		if !ok {
-			http.Error(w, `User with this email doesn't exist`, 400)
-			return
-		}
-
-		if user.Password != loginInput.Password {
-			http.Error(w, `Invalid password`, 400)
-			return
-		}
-
-		SID := GenerateSessionCookie()
-
-		api.sessions[SID] = id
-
-		cookie := &http.Cookie{
-			Name:    cookieSessionName,
-			Value:   SID,
-			Expires: time.Now().Add(10 * time.Hour),
-		}
-		http.SetCookie(w, cookie)
-	}
-
-	if r.Method == http.MethodDelete {
-		session, err := r.Cookie(cookieSessionName)
-		if err == http.ErrNoCookie {
-			http.Error(w, `Access denied`, 401)
-			return
-		}
-
-		_, ok := api.sessions[session.Value]
-		if !ok {
-			http.Error(w, `Access denied`, 401)
-			return
-		}
-
-		delete(api.sessions, session.Value)
-
-		session.Expires = time.Now().AddDate(0, 0, -1)
-		http.SetCookie(w, session)
-	}
 }
 
 func DefaultHandle(w http.ResponseWriter, r *http.Request) {
@@ -105,7 +23,7 @@ func main() {
 	}
 
 	router.HandleFunc("/session", apiSession.SessionHandle)
-	router.HandleFunc("/user", DefaultHandle)
+	router.HandleFunc("/user", apiSession.UserHandle)
 	router.HandleFunc("/profile", DefaultHandle)
 	router.HandleFunc("/restaurants", DefaultHandle)
 	router.HandleFunc("/restaurants/{restaurant_id:[0-9]+}", DefaultHandle)
--- user.go
+++ user.go
@@ -1,13 +1,35 @@
 package main
 
-import "sync"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"regexp"
+	"sync"
+	"time"
+)
 
 type User struct {
-	Email        string `json:"username"`
+	Email        string `json:"email"`
 	Password     string `json:"password"`
 	FirstName    string `json:"firstName"`
 	LastName     string `json:"lastName"`
 	ProfilePhoto string `json:"profilePhoto"`
+}
+
+const cookieSessionName = "SCDSESSIONID"
+
+type LoginInput struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type SessionHandler struct {
+	sessions  map[string]uint
+	userStore *UserStore
 }
 
 type UserStore struct {
@@ -32,7 +54,38 @@ func NewUserStore() *UserStore {
 	}
 }
 
+func (user User) IsValid() error {
+	if user.Email == "" {
+		err := errors.New("Email field is empty")
+		return err
+	}
+	ok, _ := regexp.MatchString(`\w+[@]\w+[.]\w+`, user.Email)
+	if !ok {
+		err := errors.New("Email field invalid")
+		return err
+	}
+	if user.Password == "" {
+		err := errors.New("Password field is empty")
+		return err
+	}
+	if user.FirstName == "" {
+		err := errors.New("FirstName field is empty")
+		return err
+	}
+	if user.LastName == "" {
+		err := errors.New("LastName field is empty")
+		return err
+	}
+
+	return nil
+}
+
 func (uStore *UserStore) AddUser(in *User) (uint, error) {
+	err := in.IsValid()
+	if err != nil {
+		return 0, err
+	}
+
 	uStore.mu.Lock()
 	id := uStore.nextID
 	uStore.users[id] = in
@@ -53,4 +106,106 @@ func (uStore *UserStore) GetUserByEmail(email string) (uint, *User, bool) {
 		}
 	}
 	return 0, nil, false
+}
+
+var (
+	letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+)
+
+func GenerateSessionCookie() string {
+	byteSlice := make([]rune, 64)
+	for i := range byteSlice {
+		byteSlice[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(byteSlice)
+}
+
+func (api *SessionHandler) SessionHandle(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		decoder := json.NewDecoder(r.Body)
+		loginInput := new(LoginInput)
+		err := decoder.Decode(loginInput)
+		if err != nil {
+			log.Printf("Error while unmarshalling JSON: %s", err)
+			http.Error(w, `Invalid JSONData`, 400)
+			return
+		}
+
+		id, user, ok := api.userStore.GetUserByEmail(loginInput.Email)
+		if !ok {
+			http.Error(w, `User with this email doesn't exist`, 400)
+			return
+		}
+
+		if user.Password != loginInput.Password {
+			http.Error(w, `Invalid password`, 400)
+			return
+		}
+
+		SID := GenerateSessionCookie()
+
+		api.sessions[SID] = id
+
+		cookie := &http.Cookie{
+			Name:    cookieSessionName,
+			Value:   SID,
+			Expires: time.Now().Add(10 * time.Hour),
+		}
+		http.SetCookie(w, cookie)
+	}
+
+	if r.Method == http.MethodDelete {
+		session, err := r.Cookie(cookieSessionName)
+		if err == http.ErrNoCookie {
+			http.Error(w, `Access denied`, 401)
+			return
+		}
+
+		_, ok := api.sessions[session.Value]
+		if !ok {
+			http.Error(w, `Access denied`, 401)
+			return
+		}
+
+		delete(api.sessions, session.Value)
+
+		session.Expires = time.Now().AddDate(0, 0, -1)
+		http.SetCookie(w, session)
+	}
+}
+
+func (api *SessionHandler) UserHandle(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		decoder := json.NewDecoder(r.Body)
+		userInput := new(User)
+		err := decoder.Decode(userInput)
+		if err != nil {
+			log.Printf("Error while unmarshalling JSON: %s", err)
+			http.Error(w, `Invalid JSONData`, 400)
+			return
+		}
+
+		fmt.Println(userInput)
+		var id uint
+		id, err = api.userStore.AddUser(userInput)
+		if err != nil {
+			log.Printf("Error while Add user: %s", err)
+			http.Error(w, err.Error(), 400)
+			return
+		}
+
+		SID := GenerateSessionCookie()
+
+		api.sessions[SID] = id
+
+		cookie := &http.Cookie{
+			Name:    cookieSessionName,
+			Value:   SID,
+			Expires: time.Now().Add(10 * time.Hour),
+		}
+		http.SetCookie(w, cookie)
+		for key, value := range api.userStore.users {
+			fmt.Println(key, value)
+		}
+	}
 }
