67b00ed9ff80d6f1aaa32e861038e2bf26c2d4a9
Philipp Ispolatov <philipp-is@yandex.ru>
2020-02-26 15:46:19 +0300

    cors fix

--- internal/middlewares/middlewares.go
+++ internal/middlewares/middlewares.go
@@ -8,7 +8,13 @@ type MWController struct {}
 
 func (mw *MWController) CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		origin := r.Header.Get("Origin")
+
+		if origin == "http://localhost:3000" || origin == "http://127.0.0.1:3000" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
+		//w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
